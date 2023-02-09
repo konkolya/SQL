e9fd493912c76e959363a37111f8b123f156cbcb
konkolya <104262766+konkolya@users.noreply.github.com>
2023-02-09 21:03:34 +0100

    Update LA_census_2010.sql

--- LA_census_2010.sql
+++ LA_census_2010.sql
@@ -2,17 +2,27 @@
 
 --Before creating visuals in Tableau, I ran SQL to query some basic demographic data as follows.
 
+
+--Finding the average number of population in LA
+
 SELECT AVG(totalpopulation) 
   FROM Census;
+
+
+--Listing the number of males and females
 
 SELECT SUM(totalmales),
        SUM(totalfemales) 
   FROM Census;
 
 
+--Finding the average household size
+
 SELECT AVG(avghouseholdsize) 
   FROM Census;
 
+
+--Finding all details about areas where the average age is above 60.
 
 SELECT zipcode,
        medianage,
